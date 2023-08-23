6ba951a7cd585670f23d5921772390e66b2ec3c9
AzumattDev <azumattgaming@gmail.com>
2023-08-23 18:37:32 -0400

    1.0.4

--- Patches.cs
+++ Patches.cs
@@ -1,10 +1,11 @@
 ﻿using HarmonyLib;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 namespace ChangelogEditor;
 
-[HarmonyPatch(typeof(ChangeLog),nameof(ChangeLog.Start))]
+[HarmonyPatch(typeof(ChangeLog), nameof(ChangeLog.Start))]
 static class ChangeLogStartPatch
 {
     static bool Prefix(ChangeLog __instance)
@@ -21,17 +22,28 @@ static class ChangeLogStartPatch
         if (ChangelogEditorPlugin.shouldChangeText.Value != ChangelogEditorPlugin.Toggle.On) return true;
         __instance.m_textField.text = ChangelogEditorPlugin.customFileText + __instance.m_changeLog.text;
         return false;
-
     }
-    
+
     static void Postfix(ChangeLog __instance)
     {
+        ChangeLogExtension.topicTmp = Utils.FindChild(__instance.transform, "Topic").GetComponent<TextMeshProUGUI>();
         __instance.UpdateChangelog();
+    }
+}
+
+[HarmonyPatch(typeof(ChangeLog), nameof(ChangeLog.LateUpdate))]
+static class ChangeLogLateUpdatePatch
+{
+    static void Postfix(ChangeLog __instance)
+    {
+        ChangeLogExtension.UpdateTopicText(__instance); // Not sure why this is needed after Hildir's patch, but it is. Didn't really care to investigate. Didn't need it before.
     }
 }
 
 public static class ChangeLogExtension
 {
+    internal static TextMeshProUGUI topicTmp = null!;
+
     public static void UpdateChangelog(this ChangeLog __instance)
     {
         __instance.m_hasSetScroll = false;
@@ -48,12 +60,12 @@ public static class ChangeLogExtension
         {
             __instance.m_textField.text = __instance.m_changeLog.text;
         }
-        __instance.gameObject.SetActive(ChangelogEditorPlugin.shouldShowChangelog.Value == ChangelogEditorPlugin.Toggle.On);
 
+        __instance.gameObject.SetActive(ChangelogEditorPlugin.shouldShowChangelog.Value == ChangelogEditorPlugin.Toggle.On);
     }
 
     public static void UpdateTopicText(ChangeLog clog)
     {
-        Utils.FindChild(clog.transform, "Topic").GetComponent<Text>().text = ChangelogEditorPlugin.topicText.Value;
+        topicTmp.text = ChangelogEditorPlugin.topicText.Value;
     }
 }
--- Plugin.cs
+++ Plugin.cs
@@ -19,7 +19,7 @@ namespace ChangelogEditor
     public class ChangelogEditorPlugin : BaseUnityPlugin
     {
         internal const string ModName = "ChangelogEditor";
-        internal const string ModVersion = "1.0.3";
+        internal const string ModVersion = "1.0.4";
         internal const string Author = "Azumatt";
         private const string ModGUID = Author + "." + ModName;
         private static string ConfigFileName = ModGUID + ".cfg";
